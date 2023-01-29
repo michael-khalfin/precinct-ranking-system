9f97816ce1e46a09090dc0ccca615d8c1a250571
Michael Khalfin <46636135+michael-khalfin@users.noreply.github.com>
2023-01-28 18:38:24 -0600

    better age ranges

--- stats.py
+++ stats.py
@@ -10,8 +10,7 @@ mail = pd.read_pickle('mail.pkl')
 # gender and age
 male = 0
 female = 0
-age = {}
-ranges = ['18-30', '30-40', '40-50','50-60','60+']
+ages = [0] * 20
 
 arr = [calls, walk, text, mail] 
 for i in range(3):
@@ -21,34 +20,13 @@ for i in range(3):
             female += 1
         elif cell == 'M':
             male += 1
-        cell = row['Age']
-        if 18 <= cell < 30:
-            if '18-30' in age:
-                age['18-30'] += 1
-            else:
-                age['18-30'] = 1
-        elif 30 <= cell < 40:
-            if '30-40' in age:
-                age['30-40'] += 1
-            else:
-                age['30-40'] = 1
-        elif 40 <= cell < 50:
-            if '40-50' in age:
-                age['40-50'] += 1
-            else:
-                age['40-50'] = 1
-        elif 50 <= cell < 60:
-            if '50-60' in age:
-                age['50-60'] += 1
-            else:
-                age['50-60'] = 1
-        else:
-            if '60+' in age:
-                age['60+'] += 1
-            else:
-                age['60+'] = 1
-                
-fig = plt.figure(figsize=(10,5))
-plt.plot(ranges, [age[key] for key in ranges])
+        try:
+            cell = int(row['Age'])
+            ages[cell//5] += 1
+        except:
+            pass
+
+fig = plt.figure(figsize=(15,5))
+plt.bar(['{}-{}'.format(i*5, i*5+4) for i in range(20)], ages)
 plt.xlabel('Age Range')
 plt.ylabel('Frequency')
